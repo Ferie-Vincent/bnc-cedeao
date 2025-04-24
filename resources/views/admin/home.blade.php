9dcd9208dc7c633fde288cb83abf67dd5f0ab3eb
Ferie-Vincent <145269921+Ferie-Vincent@users.noreply.github.com>
2025-04-24 17:45:10 +0000

    Update home.blade.php

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -355,7 +355,7 @@
               </td>
               <td>
                 <!--{!! Str::limit($avis['contenu'], 100) !!}-->
-                Bonjour
+{{$avis['contenu']}}
               </td>
               <td class="text-center">
                 <a href="/storage/app/public/uploads/{{$avis['fichierPDF'] }}">
